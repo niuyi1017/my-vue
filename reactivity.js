4455b56973b5a6a0efba1b1dba1703ef84d08baf
niuyi1017 <1096196462@qq.com>
2024-08-08 17:19:16 +0800

    feat: Add reactive effect and track/trigger functionality

--- reactivity.js
+++ reactivity.js
@@ -2,33 +2,53 @@
 const targetMap = new WeakMap()
 let activeEffect = null
 
-const effect = fn => {
-  activeEffect = fn
-  fn()
+function effect(fn) {
+  const effectFn = () => {
+    cleanup(effectFn)
+    activeEffect = effectFn
+    fn()
+  }
+  effectFn.deps = []
+  effectFn()
+
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    deps.delete(effectFn)
+  }
+  effectFn.deps = []
 }
 
 const track = (target, key) => {
-  console.log('track')
-  let depsMap = targetMap.get(target)
-  if (!depsMap) {
-    targetMap.set(target, (depsMap = new Map()))
-  }
-  let deps = depsMap.get(key)
-  if (!deps) {
-    depsMap.set(key, (deps = new Set()))
-  }
+  // console.log('track')
   if (activeEffect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+      depsMap.set(key, (deps = new Set()))
+    }
+
     deps.add(activeEffect)
+    activeEffect.deps.push(deps)
   }
+
 
 }
 
 const trigger = (target, key) => {
-  console.log('trigger')
+  // console.log('trigger')
   const depsMap = targetMap.get(target)
   if (depsMap) {
     const deps = depsMap.get(key)
-    deps && deps.forEach(effect => effect());
+    if (deps) {
+      const effectsToRun = new Set(deps)
+      effectsToRun && effectsToRun.forEach(effect => effect());
+    }
   }
 }
 
@@ -47,13 +67,19 @@ const reactive = (target) => {
 }
 
 const obj = reactive({
+  ok: true,
   name: 'Jack'
 })
 
+
 effect(() => {
-  console.log("effect", obj.name)
+  console.log("effect run")
+  console.log(obj.ok ? obj.name : 'not ok')
 })
 
 
-console.log(obj.name)
-obj.name = "Tom"
+// obj.name = '123'
+obj.ok = false
+obj.name = '456'
+
+
