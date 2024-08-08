09def0e3ce6eb2f159e77f28218b29286f5fca6d
niuyi1017 <1096196462@qq.com>
2024-08-08 15:53:29 +0800

    track & trigger

--- reactivity.js
+++ reactivity.js
@@ -1,11 +1,44 @@
+
+const targetMap = new WeakMap()
+
+const effect = () => {
+  console.log('effect')
+}
+
+const track = (target, key) => {
+  console.log('track')
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()))
+  }
+  deps.add(effect)
+
+}
+
+const trigger = (target, key) => {
+  console.log('trigger')
+  const depsMap = targetMap.get(target)
+  if (depsMap) {
+    const deps = depsMap.get(key)
+    deps && deps.forEach(effect => effect());
+  }
+
+
+
+}
+
 const reactive = (target) => {
   const handler = {
     get(target, key) {
-      console.log("get!")
+      track(target, key)
       return target[key]
     },
     set(target, key, value) {
-      console.log('set!')
+      trigger(target, key)
       target[key] = value
     }
   }
